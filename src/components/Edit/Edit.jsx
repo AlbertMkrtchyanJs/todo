5201a4e77b779cb084436f24c9404334565d38d8
AlbertMkrtchyanJs <albert.mkrtchyan2008@gmail.com>
2025-02-09 17:05:53 +0400

    first

--- src/components/Edit/Edit.jsx
+++ src/components/Edit/Edit.jsx
@@ -1,10 +1,32 @@
+import { useState } from "react"
 
 
 function Edit(props){
+    const [isEdited,setIsEdited] = useState(false)
+    const [text,setText] = useState('')
+
+    const doubleClick = () => {
+        setIsEdited(true)
+    };
+
+    const change = (e) => {
+        setText(e.target.value)
+    };
+
     return(
-        <>
-        <input type="text" value={props.todo.title} className="dbClick"/>
-        </>
+    <div>
+    {
+    isEdited ? <input
+             type="text"
+             value={text}
+             onChange={change}
+             
+             /> : <p
+                 onDoubleClick={doubleClick}>
+                    {props.todo.title}
+                    </p>
+        }
+    </div>   
     )
 }
 
--- src/components/InpBox/InpBox.jsx
+++ src/components/InpBox/InpBox.jsx
@@ -9,7 +9,7 @@ function InpBox(props) {
           return(
             <li className='li' key={todo.id}>
               <input className='checked' type="checkbox" checked={todo.isDone} onChange={() => props.change(todo.id)}/>
-              <span className='title' onDoubleClick={() => <Edit todo ={todo}/>}>{todo.title}</span>
+              <span className='title'><Edit title = {todo.title}/></span>
               <button className='but' onClick={() => props.remove(todo.id)}>X</button>
             </li>
           )
